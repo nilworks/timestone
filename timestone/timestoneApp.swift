8426424ed5ea65353c7ff927842557e54a512ad2
rmfls0606 <rmfls0606@naver.com>
2024-12-19 14:55:15 +0900

    프로젝트 생성

---
+++ timestone/timestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  timestoneApp.swift
+//  timestone
+//
+//  Created by 이상민 on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct timestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
