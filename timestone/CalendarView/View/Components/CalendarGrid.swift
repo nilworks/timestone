b9d6af354f7912446159aededc26ab00907e231b
avan31017 <avan31017@gmail.com>
2024-12-20 23:23:07 +0900

    CalendarView/View/Components 폴더 및 CalendarGrid 파일 추가

---
+++ timestone/CalendarView/View/Components/CalendarGrid.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarGrid.swift
+//  timestone
+//
+//  Created by 조성빈 on 12/20/24.
+//
+
+import SwiftUI
+
+struct CalendarGrid: View {
+    var body: some View {
+        // LazyVGrid, LazyHGrid 사용해서 만들어보자~
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarGrid()
+}
