6c197e72e4b103c89a6720e875818a9472ad65e6
Jinx <jinxwift@gmail.com>
2024-12-21 22:12:55 +0900

    [Feat] #1 Introduced "AddEvent" Folder #2 Composed "SetEventView" UIView #3 Set up Link View

---
+++ timestone/CalendarView/View/AddEvent/SetEventView.swift
@@ -0,0 +1,49 @@
+//
+//  SetEventView.swift
+//  timestone
+//
+//  Created by Jude Song on 12/21/24.
+//
+
+import SwiftUI
+
+struct SetEventView: View {
+    
+    @State var inputLink: String = ""
+    @State var addLocation: String = ""
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "link")
+                .foregroundStyle(.primary100)
+                .padding(.trailing, 10)
+            Text("링크")
+                .foregroundStyle(.white)
+                .font(.bodyRegular)
+                .padding(.trailing, 30)
+            TextField(
+                "링크를 입력해 주세요.",
+                text: $inputLink,
+                prompt: Text("링크를 입력해 주세요.")
+                    .foregroundColor(.netural50)
+                )
+            .padding(10)
+            .foregroundStyle(.white)
+            .font(.captionLight)
+            .frame(width: 260, height: 21, alignment: .leading)
+            .background(.netural80)
+            .cornerRadius(4)//추후 코드 수정 필요
+
+        }
+        HStack {
+            Image(systemName: "map")
+            Text("장소")
+            TextField("위치를 입력해 주세요.", text: $addLocation)
+        }
+        
+    }
+}
+
+#Preview {
+    SetEventView()
+}
