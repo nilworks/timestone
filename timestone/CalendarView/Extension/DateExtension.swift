9f5e3dabc302c3dd6ef74d66033d365aa5d45aa1
avan31017 <avan31017@gmail.com>
2024-12-22 00:05:09 +0900

    [Feat] DateExtension 월 시작일, 일 개수, 1일 요일 함수 생성

--- timestone/CalendarView/Extension/DateExtension.swift
+++ timestone/CalendarView/Extension/DateExtension.swift
@@ -5,3 +5,28 @@
 //  Created by 조성빈 on 12/21/24.
 //
 
+import SwiftUI
+
+extension Date {
+    
+    // 특정 월의 시작 일
+    func startOfMonth(month: Date) -> Date {
+        let components = Calendar.current.dateComponents([.year, .month], from: month)
+        
+        return Calendar.current.date(from: components)!
+    }
+    
+    // 특정 월에 존재하는 일 개수
+    func numberOfDays(date: Date) -> Int {
+        return Calendar.current.range(of: .day, in: .month, for: date)?.count ?? 0
+    }
+    
+    // 특정 월/1일의 요일
+    func firstWeekdayOfMonth(date: Date) -> Int {
+        let components = Calendar.current.dateComponents([.year, .month], from: date)
+        let firstDayOfMonth = Calendar.current.date(from: components)!
+        
+        return Calendar.current.component(.weekday, from: firstDayOfMonth)
+    }
+    
+}
