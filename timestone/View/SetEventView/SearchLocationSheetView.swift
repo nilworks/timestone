601081d5139a390863f074aa54055585490dad14
rmfls0606 <rmfls0606@naver.com>
2025-03-18 16:49:08 +0900

    [Add] 장소/위치 검색하는 뷰 파일 생성

---
+++ timestone/View/SetEventView/SearchLocationSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchLocationSheetView.swift
+//  timestone
+//
+//  Created by 이상민 on 3/18/25.
+//
+
+import SwiftUI
+
+struct SearchLocationSheetView: View {
+    var body: some View {
+        Color.red
+    }
+}
+
+#Preview {
+    SearchLocationSheetView()
+}
