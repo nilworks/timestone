4e73ef04a1afc0d3ef31a4041fe85dacce2ad013
rmfls0606 <rmfls0606@naver.com>
2025-03-21 19:08:42 +0900

    [Feat] 위치 검색 결과 화면 UI 코드 작성 중

--- timestone/View/SetEventView/SearchLocationSheetView.swift
+++ timestone/View/SetEventView/SearchLocationSheetView.swift
@@ -31,8 +31,36 @@ struct SearchLocationSheetView: View {
             .clipShape(RoundedRectangle(cornerRadius: 10))
             .padding(.horizontal, 15)
             
+            Divider()
+                .padding(.vertical, 10)
+                .background(.red)
+            
+            List{
+                Section{
+                    Text("안녕하시렵니까?>")
+                }
+                .background(.green)
+                
+                Section("지도 위치") {
+                    LazyVStack(
+                        alignment: .leading,
+                        spacing: 10,
+                        pinnedViews: .sectionHeaders) {
+                            ForEach(1..<100){_ in
+                                HStack{
+                                    Image(systemName: "paperplane.circle.fill")
+                                    Text("현재 위치")
+                                }//: HSTACK
+                                .frame(height: 40)
+                            }//: LOOP
+                        }//: LazyVStack
+                }//: SECTION
+                .background(.green)
+            }//: LIST
+            .background(.orange)
+            
             Spacer()
-        }
+        }//: VSTACK
         .background(.neutral90)
         .navigationTitle("위치")
         .navigationBarTitleDisplayMode(.inline)
