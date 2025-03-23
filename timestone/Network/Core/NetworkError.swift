8f03397c70862eb4d1af9dcf35c1db8ded480574
rmfls0606 <rmfls0606@naver.com>
2025-03-23 23:41:40 +0900

    [Feat] API 요청을 위한 에러 타입(NetworkError) 정의

---
+++ timestone/Network/Core/NetworkError.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkError.swift
+//  timestone
+//
+//  Created by 이상민 on 3/23/25.
+//
+
+import Foundation
+
+enum NetworkError: Error, LocalizedError{
+    case invalidURL
+    case invalidResponseType
+    case requestFailed(statusCode: Int)
+    case decondingFailed
+    case unknownResponse
+    
+    var errorDescription: String?{
+        switch self{
+        case .invalidURL:
+            return "유효하지 않은 URL입니다."
+        case .invalidResponseType:
+            return "서버로부터 받은 응답 형식이 올바르지 않습니다."
+        case .requestFailed(let statusCode):
+            return "요청이 실패했습니다. (상태코드: \(statusCode))"
+        case .decondingFailed:
+            return "응답 데이터를 파싱하는 데 실패했습니다."
+        case .unknownResponse:
+            return "알 수 없는 오류가 발생했습니다."
+        }
+    }
+}
---
+++ timestone/Network/Core/NetworkURL.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkURL.swift
+//  timestone
+//
+//  Created by 이상민 on 3/23/25.
+//
+
+import Foundation
