022461a4b2cc0fd3ab97b20d183d5d4b44d2fec0
rmfls0606 <rmfls0606@naver.com>
2025-03-24 00:04:59 +0900

    [Feat] API 요청 NetworkManager 구현

---
+++ timestone/Network/Core/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  timestone
+//
+//  Created by 이상민 on 3/23/25.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init(){}
+    
+    func CallbackRequest<T: Decodable>(request: NetworkRequest) async throws -> T{
+        
+        let (data, response) = try await URLSession.shared.data(from: request.url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else{
+            throw NetworkError.invalidResponseType
+        }
+        
+        guard (200..<300).contains(httpResponse.statusCode) else{
+            throw NetworkError.requestFailed(statusCode: httpResponse.statusCode)
+        }
+        
+        do{
+            return try JSONDecoder().decode(T.self, from: data)
+        }catch{
+            throw NetworkError.decondingFailed
+        }
+    }
+}
--- timestone/Network/Core/NetworkURL.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkURL.swift
-//  timestone
-//
-//  Created by 이상민 on 3/23/25.
-//
-
-import Foundation
