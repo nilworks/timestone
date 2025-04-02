f5577910548720a1ee1a4bb4a63725167a857da0
rmfls0606 <rmfls0606@naver.com>
2025-04-02 22:44:20 +0900

    [Fix] 장소 검색 화면에서 Switch문을 사용하여 조건에 맞는 뷰가 나오게 코드 수정

--- timestone/View/SetEventView/SearchLocationSheetView.swift
+++ timestone/View/SetEventView/SearchLocationSheetView.swift
@@ -18,76 +18,12 @@ struct SearchLocationSheetView: View {
             KakaoMapView(draw: $kakaoMapDraw)
             
             VStack{
-                HStack{
-                    if isSearching{
-                        Button {
-                            isSearching = false
-                        } label: {
-                            Image(systemName: "chevron.left")
-                                .foregroundStyle(.neutral50)
-                        }
-                        
-                    }else{
-                        Image(systemName: "magnifyingglass")
-                            .foregroundStyle(.neutral50)
-                    }
-                    
-                    TextField("위치 입력", text: $viewModel.searchLocationText)
-                        .foregroundStyle(.white)
-                        .onChange(of: viewModel.searchLocationText) { newValue in
-                            viewModel.fetchSearchLocation()
-                        }
-                    
-                    if !viewModel.searchLocationText.isEmpty{
-                        Image(systemName: "xmark.circle.fill")
-                            .foregroundStyle(.neutral50)
-                    }
-                }//: HSTACK
-                .font(.subBodyRegular)
-                .padding(10)
-                .background(.neutral80)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                .padding(.horizontal, 15)
-                .onTapGesture {
-                    isSearching = true
-                }
-                
-                if isSearching{
-                    ScrollView(.vertical) {
-                        LazyVStack(alignment: .leading, spacing: 0, pinnedViews: .sectionHeaders) {
-                            Section{
-                                HStack{
-                                    Image(systemName: "paperplane.circle.fill")
-                                    Text("현재 위치")
-                                }//: HSTACK
-                                .font(.bodyMedium)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .frame(height: 60)
-                                .overlay(alignment: .top) {
-                                    Divider()
-                                }
-                                .overlay(alignment: .bottom) {
-                                    Divider()
-                                }
-                            }//: SECTION
-                            .padding(.leading, 15)
-                            .padding(.bottom, 30)
-                            
-                            Section{
-                                ForEach(viewModel.searchResultLocation, id: \.id){ document in
-                                    SearchLocationRowView(document: document)
-                                }//: LOOP
-                            } header: {
-                                Text("지도 위치")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .background(.neutral90)
-                                    .padding(.bottom, 5)
-                            }//: SECTION
-                            .padding(.leading, 15)
-                        }//: LazyVStack
-                    }//: SCROLLVIEW
-                }else{
+                switch viewModel.viewState {
+                case .idle:
+                    MapSearchBarView()
                     Spacer()
+                case .search:
+                    LocationSearchView()
                 }
             }
         }//: ZSTACK
@@ -95,6 +31,7 @@ struct SearchLocationSheetView: View {
         .navigationTitle("위치")
         .navigationBarTitleDisplayMode(.inline)
         .animation(.linear(duration: 0.15), value: isSearching)
+        .environmentObject(viewModel)
         .onAppear {
             kakaoMapDraw = true
         }
