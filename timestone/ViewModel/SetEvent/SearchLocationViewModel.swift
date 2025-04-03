fef45e3a04932b201f12961592503e88847715fe
rmfls0606 <rmfls0606@naver.com>
2025-04-03 19:53:20 +0900

    [Feat] 사용자 위치 권한 상태 확인 및 요청 로직 추가

--- timestone/ViewModel/SetEvent/SearchLocationViewModel.swift
+++ timestone/ViewModel/SetEvent/SearchLocationViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreLocation
 
-class SearchLocationViewModel: ObservableObject{
+class SearchLocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate{
     enum ViewState {
         case idle
         case search
@@ -20,7 +20,13 @@ class SearchLocationViewModel: ObservableObject{
     @Published var locationSettingAlert: Bool = false
     
     //MARK: - 위치 매니저 생성: 위치에 관련된 대부분을 담당
-    private let locationManager = CLLocationManager()
+    lazy var locationManager = CLLocationManager()
+    
+    override init(){
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
     
     @MainActor
     func fetchSearchLocation(){
@@ -50,7 +56,39 @@ class SearchLocationViewModel: ObservableObject{
                 return
             }
             
-            // TODO: 앱에서 위치 권한 확인
+            self.checkCurrentLocation()
         }
+    }
+    
+    //MARK: - 현재 사용자의 위치 권한 상태 확인
+    func checkCurrentLocation(){
+        print(#function)
+        let status = locationManager.authorizationStatus
+        
+        switch status{
+        case .notDetermined:
+            print("이 권환에서만 권환 문구 띄울 수 있음")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            locationSettingAlert = true
+        case .authorizedWhenInUse, .authorizedAlways:
+            locationManager.startUpdatingLocation()
+        @unknown default:
+            print("오류 발생")
+        }
+    }
+    
+    //MARK: - 사용자의 권한상태가 변경될 때
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkDeviceLocation()
+    }
+    
+    //MARK: - 사용자의 위치를 성공적으로 가지고 오지 못한 경우
+    //ex) 사용자가 허용 안함 / 자녀 보호 기능
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: any Error
+    ) {
+        print(error.localizedDescription)
     }
 }
