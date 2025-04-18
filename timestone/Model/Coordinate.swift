451cf647d88d1a6cb89c74c4978ae78cd9cc6d30
rmfls0606 <rmfls0606@naver.com>
2025-04-18 16:49:27 +0900

    [Feat] 선택된 장소의 정보를 정의하기 위한 SelectedCoordinate 모델 정의

--- timestone/Model/Coordinate.swift
+++ timestone/Model/Coordinate.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+struct SelectedCoordinate{
+    let placeName: String
+    let address: String
+    let coordinate: Coordinate
+}
+
 struct Coordinate{
     let latitude: Double
     let longitude: Double
