6f2145d38d95bc485ca7d06407c7e6b802332649
rmfls0606 <rmfls0606@naver.com>
2025-06-22 02:37:15 +0900

    [Feat] 제한된 접근일 떄 보여줄 GridView(ImagePickerView)에 보여줄 썸네일 뷰 생성

---
+++ timestone/View/SetEventView/PhotoThumbnailView.swift
@@ -0,0 +1,37 @@
+//
+//  PhotoThumbnailView.swift
+//  timestone
+//
+//  Created by 이상민 on 6/22/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PhotoThumbnailView: View {
+    let asset: PHAsset
+    
+    @State private var image: UIImage? = nil
+    @EnvironmentObject private var imagePickerViewModel: ImagePickerViewModel
+    
+    private let dimension = UIScreen.main.bounds.width / 3 - 2
+    
+    var body: some View {
+        if let image = image{
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: dimension, height: dimension)
+                .clipped()
+        }else{
+            Rectangle()
+                .fill(Color.gray)
+                .frame(width: dimension, height: dimension)
+                .onAppear {
+                    imagePickerViewModel.loadThumbnail(asset: asset) { result in
+                        self.image = result
+                    }
+                }
+        }
+    }
+}
