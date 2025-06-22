d193ce6c9fa31a9651189a29929b95131a6ed0ec
rmfls0606 <rmfls0606@naver.com>
2025-06-22 13:25:31 +0900

    [Feat] 더 많은 사진 선택 버튼 클릭 시 사진 편집기(MultiImagePicker) 보여주도록 하는 변수 생성 및 sheet 코드 추가

--- timestone/View/SetEventView/ImagePickerView.swift
+++ timestone/View/SetEventView/ImagePickerView.swift
@@ -12,6 +12,8 @@ struct ImagePickerView: View {
     @StateObject private var photoLibraryObserver = PhotoLibraryObserver()
     @EnvironmentObject private var imagePickerViewModel: ImagePickerViewModel
     
+    @State private var showMultiImagePicker: Bool = false
+    
     private let gridItems = [GridItem(.flexible(), spacing: 2),
                              GridItem(.flexible(), spacing: 2),
                              GridItem(.flexible(), spacing: 2)]
@@ -23,7 +25,7 @@ struct ImagePickerView: View {
                     VStack(spacing: 0) {
                         //더 많은 사진 선택으로 이동
                         Button {
-                            // TODO: Picker 나오게 하기
+                            self.showMultiImagePicker = true
                         } label: {
                             HStack{
                                 Image(systemName: "camera")
@@ -116,6 +118,9 @@ struct ImagePickerView: View {
                 }
             }//: TOOLBAR
         }//: NAVIGATIONVIEW
+        .sheet(isPresented: $showMultiImagePicker) {
+            MultiImagePicker()
+        }
     }
 }
 
